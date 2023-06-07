d689aa4fca4fd30d63891cc6da379a4670f5628e
Replit user <>
2023-06-07 10:55:17 +0000

    projet 01

--- Coures/02_variables/variable.py
+++ Coures/02_variables/variable.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-"""
-il s'agit d'un fichier de cour pour les varaibles en python
-"""
+# """
+# il s'agit d'un fichier de cour pour les varaibles en python
+# """
 
 # pour definir un variable en pytohn
 a = 2
+print("a = ", end="")
+print(a)
--- Projets/01_module_svg/svg.py
+++ Projets/01_module_svg/svg.py
@@ -12,9 +12,9 @@ from collections import namedtuple
 # Definition de la structure Point composée de deux attributs x et y
 Point = namedtuple("Point", "x y")
 
-# À implémenter dans 'TP2. Module SVG'
+
 def genere_balise_debut_image(largeur, hauteur):
-    """
+  """
     Retourne la chaîne de caractères correspondant à la balise ouvrante pour
     décrire une image SVG de dimensions largeur x hauteur pixels. Les paramètres
     sont des entiers.
@@ -22,24 +22,24 @@ def genere_balise_debut_image(largeur, hauteur):
     Remarque : l'origine est en haut à gauche et l'axe des Y est orienté vers le
     bas.
     """
-    # TODO
-    ...
+  # TODO
+  return f'<svg width={largeur} height={hauteur} >'
 
 
 # À implémenter dans 'TP2. Module SVG'
 def genere_balise_fin_image():
-    """
+  """
     Retourne la chaîne de caractères correspondant à la balise svg fermante.
     Cette balise doit être placée après tous les éléments de description de
     l'image, juste avant la fin du fichier.
     """
-    # TODO
-    ...
+  "</svg>"
 
 
 # À implémenter dans 'TP2. Module SVG'
-def genere_balise_debut_groupe(couleur_ligne, couleur_remplissage, epaisseur_ligne):
-    """
+def genere_balise_debut_groupe(couleur_ligne, couleur_remplissage,
+                               epaisseur_ligne):
+  """
     Retourne la chaîne de caractères correspondant à une balise ouvrante
     définissant un groupe d'éléments avec un style particulier. Chaque groupe
     ouvert doit être refermé individuellement et avant la fermeture de l'image.
@@ -53,23 +53,22 @@ def genere_balise_debut_groupe(couleur_ligne, couleur_remplissage, epaisseur_lig
     Le paramètre d'épaisseur est un nombre positif ou nul, représentant la
     largeur du tracé d'une ligne en pixels.
     """
-    # TODO
-    ...
+  return f'<g fill={couleur_remplissage} stroke ={epaisseur_ligne} stroke-width = {epaisseur_ligne} '
 
 
 # À implémenter dans 'TP2. Module SVG'
 def genere_balise_fin_groupe():
-    """
+  """
     Retourne la chaîne de caractères correspondant à la balise fermante pour un
     groupe d'éléments.
     """
-    # TODO
-    ...
+  # TODO
+  ...
 
 
 # À implémenter dans 'TP2. Module SVG'
 def genere_cercle(centre, rayon):
-    """
+  """
     Retourne la chaîne de caractères correspondant à un élément SVG représentant
     un cercle (ou un disque, cela dépend de la couleur de remplissage du groupe
     dans lequel on se trouve).
@@ -77,42 +76,42 @@ def genere_cercle(centre, rayon):
     centre est une structure de données de type Point, et rayon un nombre de
     pixels indiquant le rayon du cercle.
     """
-    # TODO
-    ...
+  # TODO
+  ...
 
 
 # À implémenter dans 'TP3. Tortue Logo'
 def genere_segment(dep, arr):
-    """
+  """
     Retourne la chaîne de caractères correspondant à un élément SVG représentant
     un segment. Ce segment relie les points dep et arr.
     """
-    # TODO
-    ...
+  # TODO
+  ...
 
 
 # À implémenter dans `TP Optionnels Échiquier`
 def genere_rectangle(top_left, width, height):
-    """
+  """
     Retourne la chaîne de caractères correspondant à un élément SVG représentant un rectangle.
     """
-    # TODO
-    ...
+  # TODO
+  ...
 
 
 # À implémenter dans `TP7. Kaléidoscope`
 def genere_polygone(points):
-    """
+  """
     Retourne la chaîne de caractères correspondant à un élément SVG
     représentant un polygone. `points` est un tableaux de points.
     """
-    # TODO
-    ...
+  # TODO
+  ...
 
 
 # À implémenter dans `TP7. Kaléidoscope`
 def genere_balise_debut_groupe_transp(niveau_opacite):
-    """
+  """
     Retourne la chaîne de caractères correspondant à une balise ouvrant un
     groupe d'éléments qui, dans son ensemble, sera partiellement transparent.
     Les éléments qui composent le groupe se masquent les uns les autres dans
@@ -120,23 +119,23 @@ def genere_balise_debut_groupe_transp(niveau_opacite):
     `niveau_opacite` doit être un nombre entre 0 et 1. Ce groupe doit être
     refermé de la même manière que les groupes définissant un style.
     """
-    # TODO
-    ...
+  # TODO
+  ...
 
 
 # À implémenter dans `TP8. Plateau`
 def genere_zone_colorie(x_min, y_min, largeur, hauteur, couleur_remplissage):
-    """
+  """
     Retourne la chaîne de caractères correspondant à un élément qui colorie une
     zone rectangulaire de la couleur indiquée
     """
-    # TODO
-    ...
+  # TODO
+  ...
 
 
 # À implémenter dans `TP8. Plateau`
 def genere_texte(x_min, y_bas, contenu, hauteur):
-    """
+  """
     Retourne la chaîne de caractères correspondant à un élément SVG représentant
     un texte. Place le texte à la position indiquée. x_min est l'abscisse du
     début du texte. y_bas est l'ordonnée de la ligne de base du texte (le bas
@@ -145,5 +144,5 @@ def genere_texte(x_min, y_bas, contenu, hauteur):
     paramètre hauteur définit la taille de police (c'est-à-dire la hauteur d'une
     ligne de texte)
     """
-    # TODO
-    ...
+  # TODO
+  ...
