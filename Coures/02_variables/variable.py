5793abdcb84ad437b44d2cd1ef715c27d8ec24c1
nebil <mhmdahmdnbyl18@gmail.com>
2023-06-18 22:15:54 +0200

    cour 1 : Done

--- Coures/02_variables/variable.py
+++ Coures/02_variables/variable.py
@@ -1,9 +1,92 @@
 #!/usr/bin/env python3
-# """
-# il s'agit d'un fichier de cour pour les varaibles en python
-# """
 
+"""
+il s'agit d'un fichier de cour pour les varaibles en python
+"""
+
+
+
+#  ------------------- 1.Définition : -------------------
+
+# Une variable en python est une zone de la mémoire de l'ordi dans la quelle  une valeur est stockée 
 # pour definir un variable en pytohn
+
 a = 2
-print("a = ", end="")
 print(a)
+
+
+
+# ------------------ 2.typage ------ -----------------------
+
+# le type d'une variable 
+print(type(a)) # la fonction type permet de connaitre le type d'une varaible 
+
+
+# en C " int a = 2 " ( typage statistique ) et en python  a = 2 ( typage dynamique )
+
+# il existe plusierues types en python : int , float , bool, str ... etc 
+
+
+# ------------------ 3. Nommage ----------------------------------
+
+# le nom d'une variable : a_z && A_Z && 1_9 &&  _ \ { for , range , from , import ...etc }
+
+# Python est sensible à la casse ie : Test && test sont #ts
+
+
+# --------------- 4. écriture spécifique  ----------------------------
+
+# le sumbole e : 
+print(1e6) # égale à : 1x 10^{6} : Notation scientifique 
+
+# spération es chiffres en python :
+nombre = 6.022_140_76 
+print(nombre)
+print(type(nombre))
+
+
+# -------------------- 5. Opérations -------------------------------------- 
+
+
+print(""" 
+    = : affectation 
+    + - * / : les opérations arithématiquede base 
+    ** : puissance 
+    // : division eniter (le quotient d'une division ) 
+    % : le reste de la division eniter 
+    += : éffectue une addition puis une affectaion 
+    -= , *= , /= : de meme 
+    
+""")
+
+
+# ---------------- 6. Conversion de type : casting  ------------------
+ 
+
+# En python on peut changer le type d'une variable a l'aide des fonctions : int() , float() , str() .. etc  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
